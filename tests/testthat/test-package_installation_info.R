a289dd3d06399f1c1679c97022cbf2026ecb2f73
Jacob Scott <jscott2718@gmail.com>
2024-02-15 19:18:13 +0000

    add test for description URL field parsing

--- tests/testthat/test-package_installation_info.R
+++ tests/testthat/test-package_installation_info.R
@@ -142,6 +142,81 @@ test_that("package_installation_info works with desc GithubRepo/GithubUsername f
 
 })
 
+
+test_that("package_installation_info works with desc GitLab fields", {
+
+  withr::local_temp_libpaths()
+  libpath <- .libPaths()[1]
+
+  pkg_dir <- file.path(libpath, "foo")
+  dir.create(pkg_dir)
+
+  writeLines(
+    con = file.path(pkg_dir, "DESCRIPTION"),
+    paste(
+      sep = "\n",
+      'Package: foo',
+      'Type: Package',
+      'Version: 0.1.0',
+      'RemoteType: gitlab',
+      'RemoteUsername: foofyfooson',
+      'RemoteRepo: foo'
+    )
+  )
+
+  info <- package_installation_info("foo")
+
+  expect_identical(info, list(
+    Available_Sources = "gitlab",
+    Package = "foo",
+    Version_Installed = "0.1.0",
+    Repository = NULL,
+    Github_Username = NULL,
+    Github_Repository = NULL,
+    Gitlab_Username   = "foofyfooson",
+    Gitlab_Repository = "foo",
+    Remote_URL = NULL,
+    Bioc_Views = NULL
+  ))
+
+})
+
+test_that("package_installation_info can pare URL field", {
+
+  withr::local_temp_libpaths()
+  libpath <- .libPaths()[1]
+
+  pkg_dir <- file.path(libpath, "foo")
+  dir.create(pkg_dir)
+
+  writeLines(
+    con = file.path(pkg_dir, "DESCRIPTION"),
+    paste(
+      sep = "\n",
+      'Package: foo',
+      'Type: Package',
+      'Version: 0.1.0',
+      'URL: https://github.com/foofyfooson/foo, https://gitlab.com/foofyfooson/foo'
+    )
+  )
+
+  info <- package_installation_info("foo")
+
+  expect_identical(info, list(
+    Available_Sources = c("github", "gitlab"),
+    Package = "foo",
+    Version_Installed = "0.1.0",
+    Repository = NULL,
+    Github_Username   = "foofyfooson",
+    Github_Repository = "foo",
+    Gitlab_Username   = "foofyfooson",
+    Gitlab_Repository = "foo",
+    Remote_URL = NULL,
+    Bioc_Views = NULL
+  ))
+
+})
+
 test_that("package_installation_info works with desc RemoteRepo/RemoteUsername fields", {
 
   withr::local_temp_libpaths()
